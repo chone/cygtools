3409b99b876ecad900c12ed2af3c284ddb0ca709
chone <chonebz@gmail.com>
2016-02-02 03:44:57 +0800

    Restruct chrome command script as a nodejs package.

--- bin/chrome/main.js
+++ bin/chrome/main.js
@@ -7,20 +7,32 @@ getUri(function(uri) {
   exec(getChromeUri() + ' ' + uri);
 });
 
+/**
+ * Returns path of file chrome.exe in Windows.
+ * @return {string}
+ */
 function getChromeUri() {
   return '%UserProfile%/AppData/Local/Google/Chrome/Application/chrome.exe';
 }
 
-function getUri(callback) {
+/**
+ * Find the command line first param as a local path in Windows or a url 
+ * of website. 
+ * @param {Function} callback Success callback. 
+ * @param {Function} fail Failure callback. 
+ */
+function getUri(callback, fail) {
   var uri = process.argv[2];
-  if (/^http/.exec(uri)) {
+  if (!uri) {
+    callback('.');
+  } else if (/^http/.exec(uri)) {
     return callback(uri);
   } else {
     exec('cygpath -m ' + uri, function(err, stdout) {
       if (stdout) {
 	callback(stdout);
       } else {
-	throw Error(err);
+	fail(err);
       }
     });
   }
